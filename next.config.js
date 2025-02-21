c742fc22b7ed12c1f9900933f791b788d346919b
sufail ahmd <sufailahammed3@gmail.com>
2025-02-22 02:21:53 +0530

    gmail configure resend...fixed local

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      domains: ['https://medconnect-billing.vercel.app/'], // Add required domains
+    },
+  };
